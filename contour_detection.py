3b4106be4d64c34a0d2d2ea629d520d12c061351
Alex <alexanderbraun@freenet.de>
2021-12-10 07:04:52 +0100

    add contour_detection.py

--- contour_detection.py
+++ contour_detection.py
@@ -1,9 +1,20 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('./Resources/Photos/park.jpg')
+img = cv.imread('./Resources/Photos/cats.jpg')
 
+blank = np.zeros(img.shape, dtype='uint8')
 
-cv.imshow('Img', img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny', canny)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(len(contours))
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
+
+cv.imshow('Img Contours', blank)
 
 cv.waitKey(0)
